8cb9811f117487acad9a2fea88f401c5c995984f
dandy <danielsbarbosa27@gmail.com>
2021-02-15 16:21:15 -0300

    Validando email

--- dandy27_store/lib/screens/base/login/login_screen.dart
+++ dandy27_store/lib/screens/base/login/login_screen.dart
@@ -1,6 +1,13 @@
+import 'package:dandy27_store/helpers/validators.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passController = TextEditingController();
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,51 +18,63 @@ class LoginScreen extends StatelessWidget {
       body: Center(
         child: Card(
           margin: const EdgeInsets.symmetric(horizontal: 16),
-          child: ListView(
-            padding: const EdgeInsets.all(16),
-            shrinkWrap: true,
-            children: [
-              TextFormField(
-                decoration: InputDecoration(hintText: 'E-mail'),
-                keyboardType: TextInputType.emailAddress,
-                autocorrect: false ,
-                validator: (email){
-                  return null;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(hintText: 'Senha'),
-                autocorrect: false ,
-                obscureText: true,
-                validator: (pass){
-                  if (pass.isEmpty || pass.length < 6)
-                    return 'Senha inválida';
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16,),
-              Align(
-                alignment: Alignment.centerRight,
-                child: FlatButton(
-                  padding: EdgeInsets.zero,
-                  onPressed: (){},
-                  child: const Text('Esqueci minha senha'),
+          child: Form(
+            key: formKey,
+            child: ListView(
+              padding: const EdgeInsets.all(16),
+              shrinkWrap: true,
+              children: [
+                TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(hintText: 'E-mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false ,
+                  validator: (email){
+                    if(!emailValid(email))
+                      return 'E-mail inválido';
+                    return null;
+                  },
                 ),
-
-              ),
-              const SizedBox(height: 16,),
-              SizedBox(
-                height: 44,
-                child: RaisedButton(onPressed: (){},
-                color: Theme.of(context).primaryColor,
-                  textColor: Colors.white,
-                child: const Text('Entrar',
-                  style: TextStyle(fontSize: 20),
+                TextFormField(
+                  controller: passController,
+                  decoration: InputDecoration(hintText: 'Senha'),
+                  autocorrect: false ,
+                  obscureText: true,
+                  validator: (pass){
+                    if (pass.isEmpty || pass.length < 6)
+                      return 'Senha inválida';
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16,),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: FlatButton(
+                    padding: EdgeInsets.zero,
+                    onPressed: (){},
+                    child: const Text('Esqueci minha senha'),
                   ),
-                ),
-              )
 
-            ],
+                ),
+                const SizedBox(height: 16,),
+                SizedBox(
+                  height: 44,
+                  child: RaisedButton(onPressed: (){
+                    if(formKey.currentState.validate()){
+                      print(emailController.text);
+
+                    }
+                  },
+                  color: Theme.of(context).primaryColor,
+                    textColor: Colors.white,
+                  child: const Text('Entrar',
+                    style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                )
+
+              ],
+            ),
           ),
         ),
       ),
