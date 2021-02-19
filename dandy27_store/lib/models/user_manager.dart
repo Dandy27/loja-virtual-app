092719bd4efd148301a14e5b58b0345cbe7b23f8
dandy <danielsbarbosa27@gmail.com>
2021-02-19 02:03:58 -0300

    Carregando o usuário ao abrir o app

--- dandy27_store/lib/models/user_manager.dart
+++ dandy27_store/lib/models/user_manager.dart
@@ -5,27 +5,42 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
 class UserManager extends ChangeNotifier {
-  final FirebaseAuth auth = FirebaseAuth.instance;
+  UserManager() {
+    _loadCurrentUser();
+  }
 
-  bool loading = false;
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseUser user;
+
+  bool _loading = false;
+
+  bool get loading => _loading;
 
   Future<void> signIn({User user, Function onFail, Function onSuccess}) async {
-    setLoading(true);
+    loading = true;
     try {
       final AuthResult result = await auth.signInWithEmailAndPassword(
           email: user.email, password: user.password);
 
-      await Future.delayed(Duration(seconds: 2));
+      this.user = result.user;
 
       onSuccess();
     } on PlatformException catch (e) {
       onFail(getErrorString(e.code));
     }
-    setLoading(false);
+    loading = false;
   }
 
-  void setLoading(bool value) {
-    loading = value;
+  set loading(bool value) {
+    _loading = value;
     notifyListeners();
+  }
+
+  Future<void> _loadCurrentUser() async {
+    final FirebaseUser currentUser = await auth.currentUser();
+    if (currentUser != null) {
+      user = currentUser;
+      print(user.uid);
+    }
   }
 }
