2f343a69336511eb55f15606579bb811761a7fc2
dandy <danielsbarbosa27@gmail.com>
2021-02-20 19:37:07 -0300

    Bloqueandoos os campos durante o cadastro

--- dandy27_store/lib/common/custom_drawer/custom_drawer.dart
+++ dandy27_store/lib/common/custom_drawer/custom_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:dandy27_store/common/custom_drawer/custon_drawer_header.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 import 'drawer_tile.dart';
 
@@ -7,28 +8,42 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
+      child: Stack(
         children: [
-          CustonDrawerHeader(),
-          DrawerTile(
-            iconData: Icons.home,
-            title: 'Inicio',
-            page: 0,
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [const Color.fromARGB(255, 203, 236, 241),
+                Colors.white], begin: Alignment.topCenter,
+                end: Alignment.bottomCenter
+              )
+            ),
           ),
-          DrawerTile(
-            iconData: Icons.playlist_add_check,
-            title: 'Produtos',
-            page: 1,
-          ),
-          DrawerTile(
-            iconData: Icons.playlist_add_check,
-            title: 'Meus pedidos',
-            page: 2,
-          ),
-          DrawerTile(
-            iconData: Icons.location_on,
-            title: 'Lojas',
-            page: 3,
+          ListView(
+            children: [
+              CustonDrawerHeader(),
+              const Divider(),
+              DrawerTile(
+                iconData: Icons.home,
+                title: 'Inicio',
+                page: 0,
+              ),
+              DrawerTile(
+                iconData: Icons.playlist_add_check,
+                title: 'Produtos',
+                page: 1,
+              ),
+              DrawerTile(
+                iconData: Icons.playlist_add_check,
+                title: 'Meus pedidos',
+                page: 2,
+              ),
+              DrawerTile(
+                iconData: Icons.location_on,
+                title: 'Lojas',
+                page: 3,
+              ),
+            ],
           ),
         ],
       ),
--- dandy27_store/lib/screens/base/signup/signup_screen.dart
+++ dandy27_store/lib/screens/base/signup/signup_screen.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class SignUpScreen extends StatelessWidget {
-
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
@@ -24,101 +23,119 @@ class SignUpScreen extends StatelessWidget {
           margin: const EdgeInsets.symmetric(horizontal: 16),
           child: Form(
             key: formKey,
-            child: ListView(
-              padding: const EdgeInsets.all(16),
-              shrinkWrap: true,
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(hintText: 'Nome Completo'),
-                  validator: (name) {
-                    if (name.isEmpty)
-                      return 'Campo obrigatório';
-                    else if (name.trim().split(' ').length <= 1)
-                      return 'Preencha seu Nome Completo';
-                    return null;
-                  },
-                  onSaved: (name) => user.name = name,
-                ),
-                const SizedBox(height: 18),
-                TextFormField(
-                  decoration: const InputDecoration(hintText: 'E-mail'),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (email) {
-                    if (email.isEmpty)
-                      return 'Campo obrigatório';
-                    else if (!emailValid(email)) return 'Email inválido';
-                    return null;
-                  },
-                  onSaved: (email) => user.email = email,
-                ),
-                const SizedBox(height: 18),
-                TextFormField(
-                  decoration: const InputDecoration(hintText: 'Senha'),
-                  obscureText: true,
-                  validator: (pass) {
-                    if (pass.isEmpty)
-                      return 'Campo obrigatório';
-                    else if (pass.length < 6) return 'Senha muito curta';
-                    return null;
-                  },
-                  onSaved: (pass) => user.password = pass,
-                ),
-                const SizedBox(height: 18),
-                TextFormField(
-                  decoration: const InputDecoration(hintText: 'Repita a Senha'),
-                  obscureText: true,
-                  validator: (pass) {
-                    if (pass.isEmpty)
-                      return 'Campo obrigatório';
-                    else if (pass.length < 6) return 'Senha muito curta';
-                    return null;
-                  },
-                  onSaved: (pass) => user.confirmPassword = pass,
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                SizedBox(
-                  height: 50,
-                  child: RaisedButton(
-                    onPressed: () {
-                      if(formKey.currentState.validate()){
-                        formKey.currentState.save();
-
-                        if(user.password != user.confirmPassword){
-                          scaffoldKey.currentState
-                              .showSnackBar(SnackBar(
-                            content: Text('Senhas não coincidem! '),
-                            backgroundColor: Colors.red,
-                          ));
-                          return;
-                        }
-                          context.read<UserManager>().singUp(
-                            user: user,
-                            onSuccess: (){
-                              Navigator.of(context).pushNamed('/login');
-
-                            },
-                            onFail: (e){
-                              scaffoldKey.currentState
-                                  .showSnackBar(SnackBar(
-                                content: Text('Falha ao cadastrar $e'),
-                                backgroundColor: Colors.red,
-                              ));
-                            }
-                          );
-                      };
-                    },
-                    color: Theme.of(context).primaryColor,
-                    disabledColor: Theme.of(context).primaryColor.withAlpha(100),
-                    textColor: Colors.white,
-                    child: Text(
-                      'Criar Conta ',
-                      style: TextStyle(fontSize: 20),
+            child: Consumer<UserManager>(
+              builder: (_, userManager, __) {
+                return ListView(
+                  padding: const EdgeInsets.all(16),
+                  shrinkWrap: true,
+                  children: [
+                    TextFormField(
+                      enabled: !userManager.loading,
+                      decoration:
+                          const InputDecoration(hintText: 'Nome Completo'),
+                      validator: (name) {
+                        if (name.isEmpty)
+                          return 'Campo obrigatório';
+                        else if (name.trim().split(' ').length <= 1)
+                          return 'Preencha seu Nome Completo';
+                        return null;
+                      },
+                      onSaved: (name) => user.name = name,
                     ),
-                  ),
-                )
-              ],
+                    const SizedBox(height: 18),
+                    TextFormField(
+                      enabled: !userManager.loading,
+                      decoration: const InputDecoration(hintText: 'E-mail'),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (email) {
+                        if (email.isEmpty)
+                          return 'Campo obrigatório';
+                        else if (!emailValid(email)) return 'Email inválido';
+                        return null;
+                      },
+                      onSaved: (email) => user.email = email,
+                    ),
+                    const SizedBox(height: 18),
+                    TextFormField(
+                      enabled: !userManager.loading,
+                      decoration: const InputDecoration(hintText: 'Senha'),
+                      obscureText: true,
+                      validator: (pass) {
+                        if (pass.isEmpty)
+                          return 'Campo obrigatório';
+                        else if (pass.length < 6) return 'Senha muito curta';
+                        return null;
+                      },
+                      onSaved: (pass) => user.password = pass,
+                    ),
+                    const SizedBox(height: 18),
+                    TextFormField(
+                      enabled: !userManager.loading,
+                      decoration:
+                          const InputDecoration(hintText: 'Repita a Senha'),
+                      obscureText: true,
+                      validator: (pass) {
+                        if (pass.isEmpty)
+                          return 'Campo obrigatório';
+                        else if (pass.length < 6) return 'Senha muito curta';
+                        return null;
+                      },
+                      onSaved: (pass) => user.confirmPassword = pass,
+                    ),
+                    SizedBox(
+                      height: 16,
+                    ),
+                    SizedBox(
+                      height: 50,
+                      child: RaisedButton(
+                        onPressed: userManager.loading
+                            ? null
+                            : () {
+                                if (formKey.currentState.validate()) {
+                                  formKey.currentState.save();
+
+                                  if (user.password != user.confirmPassword) {
+                                    scaffoldKey.currentState
+                                        .showSnackBar(SnackBar(
+                                      content: Text('Senhas não coincidem! '),
+                                      backgroundColor: Colors.red,
+                                    ));
+                                    return;
+                                  }
+                                  userManager.singUp(
+                                      user: user,
+                                      onSuccess: () {
+                                        Navigator.of(context)
+                                            .pushNamed('/login');
+                                      },
+                                      onFail: (e) {
+                                        scaffoldKey.currentState
+                                            .showSnackBar(SnackBar(
+                                          content:
+                                              Text('Falha ao cadastrar $e'),
+                                          backgroundColor: Colors.red,
+                                        ));
+                                      });
+                                }
+                              },
+                        color: Theme.of(context).primaryColor,
+                        disabledColor:
+                            Theme.of(context).primaryColor.withAlpha(100),
+                        textColor: Colors.white,
+                        child: userManager.loading
+                            ? CircularProgressIndicator(
+                                valueColor:
+                                    AlwaysStoppedAnimation(Colors.white),
+                              )
+                            : const Text(
+                                'Criar Conta ',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                      ),
+                    )
+                  ],
+                );
+              },
             ),
           ),
         ),
